d99cebe1c0109a8f9b382fc6f5ce9b272b80c333
jhoselin19 <jhoselin.ponce@tecsup.edu.pe>
2023-10-14 14:58:24 -0500

    First commit

--- App6/App6/MainPage.xaml.cs
+++ App6/App6/MainPage.xaml.cs
@@ -13,34 +13,31 @@ namespace App6
         public MainPage()
         {
             InitializeComponent();
-            //List<string> estudiantes = new List<string>();
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    estudiantes.Add("Estudiante " + i.ToString());
-            //}
-            //lvEstudiantes.ItemsSource = estudiantes;
 
 
 
-            List<Estudiante> estudiantes1= new List<Estudiante>();
-            for (int i = 0; i < 10; i++)
+            List<Estudiante> estudiantes1 = new List<Estudiante>
             {
-                estudiantes1.Add(
-                    new Estudiante
-                    {
-                        Nombres = "Nombres " + i.ToString(),
-                        Apellidos = "Apellidos " + i.ToString(),
-                        Edad = i,
-                        Seccion = (i % 3 == 0) ? "A" : (i % 3 == 1) ? "B" : "C" // Asigna secciones A, B, C
-                    }
-                    ); 
-            }
+                new Estudiante { Nombres = "Juan", Apellidos = "Pérez", Edad = 19, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "Ana", Apellidos = "García", Edad = 20, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "Carlos", Apellidos = "Rodríguez", Edad = 21, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "María", Apellidos = "Martínez", Edad = 22, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Javier", Apellidos = "Hernández", Edad = 23, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Laura", Apellidos = "López", Edad = 24, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Sergio", Apellidos = "González", Edad = 25, Seccion = "Seccion C" },
+                new Estudiante { Nombres = "Carmen", Apellidos = "Morales", Edad = 26, Seccion = "Seccion C" },
+                new Estudiante { Nombres = "Fernando", Apellidos = "Guerrero", Edad = 27, Seccion = "Seccion C" },
+                new Estudiante { Nombres = "Patricia", Apellidos = "Ramírez", Edad = 28, Seccion ="Seccion D" },
+                new Estudiante { Nombres = "Ricardo", Apellidos ="Aguilar", Edad=29, Seccion="Seccion D"},
+                new Estudiante { Nombres ="Luisa", Apellidos="Peña", Edad=30, Seccion="Seccion D"},
+                new Estudiante { Nombres ="Roberto", Apellidos="Navarro", Edad=31, Seccion="Seccion D"},
+                new Estudiante { Nombres ="Isabel", Apellidos="Romero", Edad=32, Seccion="Seccion D"},
+                new Estudiante { Nombres ="Pedro", Apellidos="Torres", Edad=33, Seccion="Seccion D"},
+            };
 
             var groupedStudents = estudiantes1.GroupBy(e => e.Seccion);
 
             lvEstudiantes1.ItemsSource = groupedStudents;
-
-
         }
     }
 }
